b9d4c4077ca9f932b3e8cba05998d90c6297670e
tike1453 <tkui123@yahoo.com>
2013-02-23 18:10:49 -0500

    Create primedef.rb

---
+++ primedef.rb
@@ -0,0 +1,4 @@
+# FLPFIN(Find the Largest Prime Factor in Number) by Tyler Kui
+# Description:
+# This project's goal is to find the largest prime factor of an inputed number
+# Target Number: 600851475143
