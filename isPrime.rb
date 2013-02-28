0358708257020231c3d3e010f8598da96b9d424d
tike1453 <tkui123@yahoo.com>
2013-02-27 22:33:10 -0500

    Create isPrime.rb

---
+++ isPrime.rb
@@ -0,0 +1,22 @@
+# Function to determine if a number is prime
+# returns true or false
+#-------------------------------------------------------------
+def isPrime(number)
+  if number % 2 == 0
+		return false
+	end
+	for num in (3..number-1).step(2)
+		if number % num == 0
+			return false
+		end
+	end
+	return true
+end
+
+=begin
+if isPrime(600851475143600851475143600851475143600851475143600851475143600851475147)
+	puts "prime"
+else
+	puts "composite"
+end
+=end
